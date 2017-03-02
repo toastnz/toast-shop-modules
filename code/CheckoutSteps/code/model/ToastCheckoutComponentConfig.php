f639b53f953523e926c0ed8c67034bd9acf1395a
Danae Miller-Clendon <danae.millerc@gmail.com>
2017-03-02 14:04:15 +1300

    Remove unnecessary pages, shuffle structure

---
+++ code/CheckoutSteps/code/model/ToastCheckoutComponentConfig.php
@@ -0,0 +1,129 @@
+<?php
+
+use SilverStripe\Omnipay\GatewayInfo;
+
+/**
+ * Class ToastCheckoutComponentConfig
+ */
+class ToastCheckoutComponentConfig extends CheckoutComponentConfig
+{
+    public function __construct(Order $order)
+    {
+        /** =========================================
+         * @var CheckoutStepConfig|SiteConfig $siteConfig
+         * ========================================*/
+
+        parent::__construct($order);
+
+        $siteConfig = SiteConfig::current_site_config();
+
+        if ($siteConfig->EnableCheckoutSteps) {
+
+            $steps = explode(',', $siteConfig->EnabledSteps);
+            $steps = array_filter($steps);
+
+            if (!empty($steps)) {
+                foreach ($steps as $step) {
+                    if (class_exists($step . 'CheckoutComponent')) {
+                        if ($step == 'Membership') {
+                            if (Checkout::member_creation_enabled() && !Member::currentUserID()) {
+                                $this->addComponent(call_user_func($step . 'CheckoutComponent::create'));
+                            }
+                            continue;
+                        } elseif ($step == 'Payment') {
+                            if (count(GatewayInfo::getSupportedGateways()) > 1) {
+                                $this->addComponent(call_user_func($step . 'CheckoutComponent::create'));
+                            }
+                        } else {
+                            $this->addComponent(call_user_func($step . 'CheckoutComponent::create'));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public function getFormFields()
+    {
+        /** =========================================
+         * @var CheckoutComponent       $component
+         * @var FieldList               $cfields
+         * ========================================*/
+
+        if (SiteConfig::current_site_config()->EnableCheckoutSteps) {
+
+            $fields = FieldList::create();
+            $pos    = 1;
+
+            foreach ($this->getComponents() as $component) {
+
+                $cname = $component->Name();
+
+                if ($cfields = $component->getFormFields($this->order)) {
+
+                    if ($cfields->count()) {
+
+                        /** -----------------------------------------
+                         * Header
+                         * ----------------------------------------*/
+
+                        $componentTitle = preg_replace('/([a-z])([A-Z])/s', '$1 $2', str_replace('CheckoutComponent', '', $cname));
+
+                        $componentData = ArrayData::create([
+                            'Title'      => _t('TOASTSHOP.' . $cname . 'Title', $componentTitle),
+                            'StepNumber' => $pos
+                        ]);
+
+                        $cfields->unshift(LiteralField::create(
+                            $cname . 'Header',
+                            $this->order->customise($componentData)->renderWith('CheckoutComponentHeader')->forTemplate()
+                        ));
+
+                        /** -----------------------------------------
+                         * Wrapper
+                         * ----------------------------------------*/
+
+                        $cfields->unshift(LiteralField::create($cname . 'Wrapper', '<div id="' . $cname . '_wrapper' . '">'));
+
+                        /** -----------------------------------------
+                         * Continue button
+                         * ----------------------------------------*/
+
+                        $cfields->push(
+                            FormAction::create($cname . '_continue', _t('TOASTSHOP.ContinueButton', 'Continue'))
+                                ->setUseButtonTag(true)
+                                ->addExtraClass('button button--secondary')
+                        );
+
+                        /** -----------------------------------------
+                         * Close
+                         * ----------------------------------------*/
+
+                        $cfields->push(LiteralField::create($cname . 'Close', '</div>'));
+
+                        $pos++;
+
+                    }
+
+                    // Merge fields
+                    $fields->merge($cfields);
+
+                } else {
+                    user_error("getFields on  " . $cname . " must return a FieldList");
+                }
+            }
+
+            return $fields;
+        } else {
+            $fields = FieldList::create();
+            foreach ($this->getComponents() as $component) {
+                if ($cfields = $component->getFormFields($this->order)) {
+                    $fields->merge($cfields);
+                } else {
+                    user_error("getFields on  " . get_class($component) . " must return a FieldList");
+                }
+            }
+            return $fields;
+        }
+    }
+}
---
+++ code/CheckoutSteps/code/model/components/AccessoriesCheckoutComponent.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Class AccessoriesCheckoutComponent
+ */
+class AccessoriesCheckoutComponent extends CheckoutComponent
+{
+    /**
+     * Get form fields for manipulating the current order,
+     * according to the responsibilty of this component.
+     *
+     * @param  Form $form the form being updated
+     *
+     * @throws Exception
+     * @return FieldList fields for manipulating order
+     */
+    public function getFormFields(Order $order)
+    {
+        $fields = FieldList::create();
+
+        $fields->push(LiteralField::create('AccessoriesThing', '<p>This is the accessories.</p>'));
+
+        return $fields;
+    }
+
+    /**
+     * Is this data valid for saving into an order?
+     *
+     * This function should never rely on form.
+     *
+     * @param array $data data to be validated
+     *
+     * @throws ValidationException
+     * @return boolean the data is valid
+     */
+    public function validateData(Order $order, array $data)
+    {
+        return true;
+    }
+
+    /**
+     * Get required data out of the model.
+     *
+     * @param  Order $order order to get data from.
+     *
+     * @return array        get data from model(s)
+     */
+    public function getData(Order $order)
+    {
+        $data = [];
+        return $data;
+    }
+
+    /**
+     * Set the model data for this component.
+     *
+     * This function should never rely on form.
+     *
+     * @param array $data data to be saved into order object
+     *
+     * @throws Exception
+     * @return Order the updated order
+     */
+    public function setData(Order $order, array $data)
+    {
+        return $order;
+    }
+}
