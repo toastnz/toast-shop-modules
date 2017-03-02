92f424fe8ce06d64e8d56db6282eaf9af506e13b
Danae Miller-Clendon <danae.millerc@gmail.com>
2017-03-03 10:12:45 +1300

    forTemplate on checkout steps

--- code/CheckoutSteps/code/model/CheckoutStepObject.php
+++ code/CheckoutSteps/code/model/CheckoutStepObject.php
@@ -1,16 +1,18 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: danae
- * Date: 2/03/17
- * Time: 3:24 PM
+ * Class CheckoutStepObject
+ *
+ * @property string Type
  */
 class CheckoutStepObject extends DataObject
 {
     private static $singular_name = 'Step';
     private static $plural_name = 'Steps';
     private static $default_sort = 'SortOrder';
+
+    /** @var CheckoutComponent $component */
+    protected $component;
 
     private static $has_one = [
         'Parent' => 'SiteConfig'
@@ -24,4 +26,24 @@ class CheckoutStepObject extends DataObject
         'SortOrder' => 'Int',
         'Type'      => 'Varchar(100)'
     ];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if ($this->exists() && $this->Type) {
+            if (class_exists($this->Type . 'CheckoutComponent')) {
+                $this->component = self::singleton($this->Type . 'CheckoutComponent');
+            }
+        }
+    }
+
+    public function forTemplate()
+    {
+        if ($this->component) {
+            return $this->renderWith([$this->Type, 'CheckoutComponent'], ['Fields' => $this->component->getFrontEndFields()]);
+        }
+
+        return '';
+    }
 }
---
+++ code/CheckoutSteps/templates/Includes/CheckoutComponent.ss
@@ -0,0 +1,5 @@
+<% include CheckoutComponentHeader %>
+
+<% loop $Fields %>
+    {$Field}
+<% end_loop %>
